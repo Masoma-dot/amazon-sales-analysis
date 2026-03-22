aeedb1959dbdcd7ecc9b89859958f863aeef9fd6
Masoma Shai <shaimasoma4@gmail.com>
2026-03-22 05:58:04 +0200

    Add additional questions to transformation.sql

--- docs/sql/02_transformation.sql
+++ docs/sql/02_transformation.sql
@@ -1,1 +1,52 @@
 -- Data transformation queries will go here
+## This is where we will perform our analysis.
+WE WILL ANSWER THE FOLLOWING QUESTIONS, GROUPS:
+1. CORE BUSINESS HEALTH
+What is the total revenue?
+What is the total number of orders?
+What is the total quantity sold?
+What is the cancellation rate?
+What is the average order value (AOV)?
+_________________________________
+2. TIME-BASED ANALYSIS
+How does revenue trend over time (daily/monthly)?
+Which days had the highest sales?
+Are there sales spikes or drops?
+How does cancellation trend over time?
+_________________________________
+3. PRODUCT ANALYSIS
+Which categories generate the most revenue?
+Which categories have the highest cancellations?
+Which products (SKU/ASIN) perform best?
+What is the average order value per category?
+_________________________________
+4. CUSTOMER/LOCATION ANALYSIS
+Which cities generate the most revenue?
+Which states perform best/worst?
+Are there cities with high cancellations?
+__________________________________
+5. OPERATIONS & LOGISTICS
+Which cities generate the most revenue?
+Which states perform best/worst?
+Are there cities with high cancellations?
+__________________________________
+6. BUSINESS DRIVERS
+Do promotions increase revenue or just volume?
+Do B2B vs B2C customers behave differently?
+What factors most influence high-value orders?
+___________________________________
+
+
+_______________________________________________________________________________________________________________________
+
+________________________________________________________________________________________________________________________
+
+________________________________________________________________________________________________________________________
+  
+__________________________________________________________________________________________________________________________
+
+__________________________________________________________________________________________________________________________
+
+__________________________________________________________________________________________________________________________
+
+__________________________________________________________________________________________________________________________
